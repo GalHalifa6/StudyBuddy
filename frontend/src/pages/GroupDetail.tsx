c272c88c5911a002f00431b1a4096715916bc363
aamit98 <aamit@post.bgu.ac.il>
2025-12-23 23:15:49 +0200

    Fix GroupDetail useEffect dependency and MyGroups ref warnings

--- frontend/src/pages/GroupDetail.tsx
+++ frontend/src/pages/GroupDetail.tsx
@@ -26,6 +26,7 @@ const GroupDetail: React.FC = () => {
     if (id) {
       fetchGroupData();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
   const fetchGroupData = async () => {
